1195fddb464eae66a97d4f29b98485446c21900f
Your Name <you@example.com>
2023-10-07 22:20:28 +0200

    ref

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ func main() {
 	for it := m.Iter(); it.HasNext(); it.Next() {
 		m.SetIt(it, float32(it.Y*100+it.X))
 	}
-	m.MulConst(0.5)
 	for it := m.Iter(); it.HasNext(); it.Next() {
 		fmt.Printf("%s %f\n", it, m.GetIt(it))
 	}
--- matrix/matrix.go
+++ matrix/matrix.go
@@ -2,36 +2,38 @@ package matrix
 
 import "fmt"
 
-// Matrix is a matrix of points interpreted as (x, y) points.
-type Matrix struct {
+type Val float32
+
+// Matrix is a matrix of points interpreted as (x, y) points, supporting linear operations.
+type Matrix[T Val] struct {
 	fields []float32
 	yrange int
 	xrange int
 }
 
-func New(xrange, yrange int) *Matrix {
-	return &Matrix{
+func New[T Val](xrange, yrange int) *Matrix[T] {
+	return &Matrix[T]{
 		fields: make([]float32, xrange*yrange),
 		xrange: xrange,
 		yrange: yrange,
 	}
 }
 
-func (m *Matrix) Fill(value float32) {
+func (m *Matrix[T]) Fill(value float32) {
 	for it := m.Iter(); it.HasNext(); it.Next() {
 		m.SetIt(it, value)
 	}
 }
 
-func (m *Matrix) SetIt(it *Iter, v float32) {
+func (m *Matrix[T]) SetIt(it *Iter[T], v float32) {
 	m.fields[it.index()] = v
 }
 
-func (m *Matrix) GetIt(it *Iter) float32 {
+func (m *Matrix[T]) GetIt(it *Iter[T]) float32 {
 	return m.fields[it.index()]
 }
 
-func (m *Matrix) Add(t *Matrix) error {
+func (m *Matrix[T]) Add(t *Matrix[T]) error {
 	if err := m.checkSameSize(t); err != nil {
 		return err
 	}
@@ -41,51 +43,41 @@ func (m *Matrix) Add(t *Matrix) error {
 	return nil
 }
 
-func (m *Matrix) AddConst(c float32) error {
+func (m *Matrix[T]) AddVal(c float32) error {
 	for it := m.Iter(); it.HasNext(); it.Next() {
 		m.SetIt(it, m.GetIt(it)+c)
 	}
 	return nil
 }
 
-func (m *Matrix) Mul(t *Matrix) error {
-	if err := m.checkSameSize(t); err != nil {
-		return err
-	}
-	for it := m.Iter(); it.HasNext(); it.Next() {
-		m.SetIt(it, m.GetIt(it)*t.GetIt(it))
-	}
-	return nil
-}
-
-func (m *Matrix) MulConst(c float32) error {
+func (m *Matrix[T]) MulConst(c float32) error {
 	for it := m.Iter(); it.HasNext(); it.Next() {
 		m.SetIt(it, m.GetIt(it)*c)
 	}
 	return nil
 }
 
-func (m *Matrix) checkSameSize(t *Matrix) error {
+func (m *Matrix[T]) checkSameSize(t *Matrix[T]) error {
 	if !(m.xrange == t.xrange && m.yrange == t.yrange) {
 		return fmt.Errorf("matrices differ in size: %s vs %s", m.getSizeString(), t.getSizeString())
 	}
 	return nil
 }
 
-func (m *Matrix) getSizeString() string {
+func (m *Matrix[T]) getSizeString() string {
 	return fmt.Sprintf("%dx%d", m.xrange, m.yrange)
 }
 
-type Iter struct {
+type Iter[T Val] struct {
 	X, Y int
-	m    *Matrix
+	m    *Matrix[T]
 }
 
-func (m *Matrix) Iter() *Iter {
-	return &Iter{m: m}
+func (m *Matrix[T]) Iter() *Iter[T] {
+	return &Iter[T]{m: m}
 }
 
-func (it *Iter) Next() {
+func (it *Iter[T]) Next() {
 	if !it.HasNext() {
 		return
 	}
@@ -96,14 +88,14 @@ func (it *Iter) Next() {
 	}
 }
 
-func (it *Iter) HasNext() bool {
+func (it *Iter[T]) HasNext() bool {
 	return it.Y < it.m.yrange
 }
 
-func (it *Iter) index() int {
+func (it *Iter[T]) index() int {
 	return it.X + it.Y*it.m.xrange
 }
 
-func (it *Iter) String() string {
+func (it *Iter[T]) String() string {
 	return fmt.Sprintf("%d,%d", it.X, it.Y)
 }
